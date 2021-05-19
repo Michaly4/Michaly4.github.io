485e241fd846c68f2b80cf81e60dff2c4db959fa
Michaly4 <74726142+Michaly4@users.noreply.github.com>
2021-05-19 22:55:33 +0200

    Update script.js

--- UFU/script.js
+++ UFU/script.js
@@ -73,5 +73,6 @@ const mainElement = document.documentElement;
 const mainElementWidth = mainElement.clientWidth;
 const mainElementHeight = mainElement.clientHeight;
 
+
 console.log(mainElementWidth);
 console.log(mainElementHeight);
